6a7bda3ca2fe8c07419d34f99aa8dc7339a0af34
gera2705 <kolosov.slava.70@gmail.com>
2021-08-17 19:32:28 +0800

    Добавлена разметка для OnBoardingFragment.kt

---
+++ app/src/main/java/com/tinkoff_sirius/koshelok/ui/OnBoardingFragment.kt
@@ -0,0 +1,32 @@
+package com.tinkoff_sirius.koshelok.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tinkoff_sirius.koshelok.R
+
+class OnBoardingFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = OnBoardingFragment()
+    }
+
+    private lateinit var viewModel: OnBoardingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.on_boarding_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OnBoardingViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
