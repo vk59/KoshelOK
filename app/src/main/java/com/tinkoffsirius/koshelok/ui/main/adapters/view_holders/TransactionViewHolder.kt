0113edfec0b9694949721d4f545cacf8dba3f46c
Ivan Kostylev <jkostylev47@gmail.com>
2021-08-22 16:09:30 +0300

    Добавлены импорты. Добавлена иконка.

--- app/src/main/java/com/tinkoffsirius/koshelok/ui/main/adapters/view_holders/TransactionViewHolder.kt
+++ app/src/main/java/com/tinkoffsirius/koshelok/ui/main/adapters/view_holders/TransactionViewHolder.kt
@@ -1,6 +1,9 @@
 package com.tinkoffsirius.koshelok.ui.main.adapters.view_holders
 
+import android.content.res.ColorStateList
+import com.tinkoffsirius.koshelok.R
 import com.tinkoffsirius.koshelok.databinding.ItemTransactionBinding
+import com.tinkoffsirius.koshelok.entities.TransactionType
 import com.tinkoffsirius.koshelok.ui.ResourceProvider
 import com.tinkoffsirius.koshelok.ui.main.adapters.model.MainItem
 
