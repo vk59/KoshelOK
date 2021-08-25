970780a600d58d1cc1cacc64f0feceb6d9fae169
Ivan Kostylev <jkostylev47@gmail.com>
2021-08-25 17:37:14 +0300

    Дополнены Extensions.kt.

--- app/src/main/java/com/tinkoffsirius/koshelok/config/Extensions.kt
+++ app/src/main/java/com/tinkoffsirius/koshelok/config/Extensions.kt
@@ -1,5 +1,6 @@
 package com.tinkoffsirius.koshelok.config
 
+import com.tinkoffsirius.koshelok.Icons
 import com.tinkoffsirius.koshelok.entities.Category
 import com.tinkoffsirius.koshelok.entities.Currency
 import com.tinkoffsirius.koshelok.entities.PosedTransaction
@@ -24,7 +25,8 @@ fun PosedTransaction.toCreateTransactionData(
 }
 
 fun CategoryData.toCategory(): Category {
-    return Category(id, TransactionType.valueOf(categoryType), name, icon, color)
+    return Category(id, TransactionType.valueOf(categoryType), name,
+        Icons.values().first { icon == it.id }.drawableId, color)
 }
 
 fun Category.toCategoryData(creationDate: LocalDateTime, userId: Long): CategoryData {
@@ -32,7 +34,7 @@ fun Category.toCategoryData(creationDate: LocalDateTime, userId: Long): Category
         id,
         categoryName,
         color,
-        icon,
+        Icons.values().first { icon == it.drawableId }.id,
         creationDate.toString(),
         typeName.name,
         userId
