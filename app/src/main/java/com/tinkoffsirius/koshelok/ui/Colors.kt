82453c57503d4c158beeac3c873f0bddaa89b410
gera2705 <kolosov.slava.70@gmail.com>
2021-08-25 22:19:13 +0800

    Добавлены анимации по бэк стеку

--- app/src/main/java/com/tinkoffsirius/koshelok/ui/Colors.kt
+++ app/src/main/java/com/tinkoffsirius/koshelok/ui/Colors.kt
@@ -2,8 +2,8 @@ package com.tinkoffsirius.koshelok.ui
 
 import com.tinkoffsirius.koshelok.R
 
-enum class Colors(val rgb: Int , var res: Int) {
-    GREEN(-7617718 , R.color.green),
-    BLUE(-10996754 , R.color.main_blue),
-    RED(-3978678, R.color.red)
+enum class Colors(val id: Int, val rgb: Int , var res: Int) {
+    GREEN(1,-7617718 , R.color.green),
+    BLUE(2, -10996754 , R.color.main_blue),
+    RED(3, -3978678, R.color.red)
 }
