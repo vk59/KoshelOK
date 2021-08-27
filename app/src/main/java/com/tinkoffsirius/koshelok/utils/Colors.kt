66884a8e597e5a1eb0c53a09e8e0da7786193c92
gera2705 <kolosov.slava.70@gmail.com>
2021-08-27 12:18:28 +0800

    Изменения по UI

--- app/src/main/java/com/tinkoffsirius/koshelok/utils/Colors.kt
+++ app/src/main/java/com/tinkoffsirius/koshelok/utils/Colors.kt
@@ -4,6 +4,11 @@ import com.tinkoffsirius.koshelok.R
 
 enum class Colors(val id: Int, val rgb: Int , var res: Int) {
     GREEN(1,-7617718 , R.color.green),
-    BLUE(2, -10996754 , R.color.main_blue),
-    RED(3, -3978678, R.color.red)
+    MAIN_BLUE(2, -10996754 , R.color.main_blue),
+    RED(3, -3978678, R.color.red),
+    PINK(4, -1166406 , R.color.pink),
+    BROWN(5 , -6731961 , R.color.brown),
+    BLUE(6, -13393938, R.color.blue),
+    PURPLE(7 , -7259779, R.color.purple),
+    ORANGE(8, -1137869 , R.color.orange)
 }
